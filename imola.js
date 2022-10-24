13e51afa9f0bff4e84bae987bfc5592f92ebe3f9
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2022-10-24 11:30:25 +0300

    Update:  seen some mistakes in the imola calculator. Fixed!

--- imola.js
+++ imola.js
@@ -8,13 +8,14 @@ export function applyDiscImola() {
   const inputValue = inputPriceImola.value;
   regularPriceImola.innerText = inputValue;
   const disc1 = (50 / 100) * inputValue;
-  const disc2 = (10 / 100) * disc1 - disc1;
-  const disc3 = (10 / 100) * disc2 - disc2;
-  const disc4 = (10 / 100) * disc3 - disc3;
-  const disc5 = (10 / 100) * disc4 - disc4;
-  const disc6 = (5 / 100) * disc5 - disc5;
-  const sell = (40 / 100) * disc6 + Math.ceil(disc6);
+  const disc2 = disc1 - (10 / 100) * disc1;
+  const disc3 = disc2 - (10 / 100) * disc2;
+  const disc4 = disc3 - (10 / 100) * disc3;
+  const disc5 = disc4 - (10 / 100) * disc4;
+  const disc6 = disc5 - (5 / 100) * disc5;
+  const sell = disc6 + (45 / 100) * disc6;
+  console.log(disc1, disc2, disc3, disc4, disc5, disc6, sell);
 
-  discPriceImola.innerText = Math.ceil(disc5);
+  discPriceImola.innerText = Math.ceil(disc6);
   sellingPriceImola.innerText = Math.ceil(sell);
 }
